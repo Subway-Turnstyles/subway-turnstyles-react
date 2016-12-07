e7fba7b1506ce0ed19f9e7015b98908b8fd73d6a
Debika Sarkar <debikasarkar713@gmail.com>
2016-12-07 11:51:11 -0500

    adding everything

---
+++ src/components/App/Landing/Landing.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './Landing.css';
+
+class Landing extends Component {
+  render(){
+    return (
+      )
+  }
+}
+
+export default Landing;
+
---
+++ src/components/App/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './Search.css';
+
+class Search extends Component {
+  render(){
+    return (
+      )
+  }
+}
+
+export default Search;
+
